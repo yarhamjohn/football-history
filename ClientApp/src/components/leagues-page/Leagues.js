547ebbbb29744cea3e34ff1c3b3974b636f714d1
John Yarham <yarhamjohn@gmail.com>
2018-11-05 21:29:15 +0000

    Change what is displayed when loading

--- ClientApp/src/components/leagues-page/Leagues.js
+++ ClientApp/src/components/leagues-page/Leagues.js
@@ -22,43 +22,45 @@ export class Leagues extends Component {
     if (this.state.loading) {
       body = <p><em>Loading...</em></p>
     } else {
-      body = <table className='table'>
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th>Team</th>
-            <th>Played</th>
-            <th>Won</th>
-            <th>Drawn</th>
-            <th>Lost</th>
-            <th>Goals For</th>
-            <th>Goals Against</th>
-            <th>Goal Diff.</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.leagueTable.leagueTableRow.map(row =>
-            <tr key={row.position}>
-              <td>{row.position}</td>
-              <td>{row.team}</td>
-              <td>{row.played}</td>
-              <td>{row.won}</td>
-              <td>{row.drawn}</td>
-              <td>{row.lost}</td>
-              <td>{row.goalsFor}</td>
-              <td>{row.goalsAgainst}</td>
-              <td>{row.goalDifference}</td>
-              <td>{row.points}</td>
+      body = <React.Fragment>
+        <h1>{this.state.leagueTable.competition} ({this.state.leagueTable.season})</h1>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Team</th>
+              <th>Played</th>
+              <th>Won</th>
+              <th>Drawn</th>
+              <th>Lost</th>
+              <th>Goals For</th>
+              <th>Goals Against</th>
+              <th>Goal Diff.</th>
+              <th>Points</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.leagueTable.leagueTableRow.map(row =>
+              <tr key={row.position}>
+                <td>{row.position}</td>
+                <td>{row.team}</td>
+                <td>{row.played}</td>
+                <td>{row.won}</td>
+                <td>{row.drawn}</td>
+                <td>{row.lost}</td>
+                <td>{row.goalsFor}</td>
+                <td>{row.goalsAgainst}</td>
+                <td>{row.goalDifference}</td>
+                <td>{row.points}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </React.Fragment>
     };
 
     return (
       <div>
-        <h1>{this.state.leagueTable.competition} ({this.state.leagueTable.season})</h1>
         {body}
       </div>
     );
