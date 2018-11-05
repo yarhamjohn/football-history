e27adb7b8b481bc1b8e02f2736fb32b94832f962
John Yarham <yarhamjohn@gmail.com>
2018-11-05 14:50:59 +0000

    Remove default pages

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Leagues } from './components/Leagues';
 
 export default class App extends Component {
@@ -14,8 +12,6 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route exact path='/leagues' component={Leagues} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
       </Layout>
     );
   }
