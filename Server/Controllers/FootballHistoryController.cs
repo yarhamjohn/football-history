bff13867d3f1ac5fb42c14730eb902a8916942af
John Yarham <yarhamjohn@gmail.com>
2018-11-05 21:02:36 +0000

    Move models to separate files

--- Server/Controllers/FootballHistoryController.cs
+++ Server/Controllers/FootballHistoryController.cs
@@ -49,26 +49,5 @@ namespace football_history.Controllers
 
             return league;
         }
-
-        public class LeagueTable
-        {
-            public string Competition { get; set; }
-            public string Season { get; set; }
-            public List<LeagueTableRow> LeagueTableRow { get; set; }
-        }
-
-        public class LeagueTableRow
-        {
-            public int Position { get; set; }
-            public string Team { get; set; }
-            public int Played { get; set; }
-            public int Won { get; set; }
-            public int Drawn { get; set; }
-            public int Lost { get; set; }
-            public int GoalsFor { get; set; }
-            public int GoalsAgainst { get; set; }
-            public int GoalDifference { get; set; }
-            public int Points { get; set; }
-        }
     }
 }
