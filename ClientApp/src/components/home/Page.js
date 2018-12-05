9e723c7ccfdca1e78f11245a5154d1fa07eb1a2f
John Yarham <yarhamjohn@gmail.com>
2018-12-05 21:44:57 +0000

    Change welcome page

--- ClientApp/src/components/home/Page.js
+++ ClientApp/src/components/home/Page.js
@@ -4,21 +4,57 @@ class Page extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>History of the English Football League</h1>
+        <p>This application provides data and analyses across each of the seasons, leagues and teams involved in the English Football League since its inception. There are now 92 clubs playing in the top 4 divisions of English Football, with an extensive non-league structure below League Two.</p>
+      
+        <h3>League structure</h3>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li>The Football league was formed in 1888 and initially consisted of a single division, the First Division.</li>
+          <li>A second tier, the Second Division, was added to the league for the 1892-93 season.</li>
+          <li>For the 1920-21 season, a third tier, the Third Division was added to the League.</li>
+          <li>The following year, the Third Division was split into North and South divisions, effectively adding a whole new division.</li>
+          <li>In 1958-59, the Third Division North/South split was converted into a single Third Division and a new Fourth Division.</li>
+          <li>The 1992-93 season saw the breakaway of the top division to form the new Premier League. Divisions Two, Three and Four were renamed to Divisions One, Two and Three.</li>
+          <li>In 2004, Divisions One, Two and Three were renamed to the Championship, League One and League Two.</li>
         </ul>
-        <p>To help you get started, we've also set up:</p>
+
+        <h3>Rules and regulations</h3>
+        <h4><em>Points</em></h4>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li>From the inception of the league in 1888, 2 points were awarded for a win and 1 for a draw.</li>
+          <li>This was changed to 3 points for a win and 1 point for a draw from the 1981-82 season.</li>
         </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      <p>test</p></div>
+        <h4><em>Rankings</em></h4>
+        <ul>
+          <li>Initially, 'Goal Average' (actually goal ratio - goals for / goals against) was used to separate teams level on points.</li>
+          <li>For the 1976-77 season, this was replaced with Goal Difference.</li>          
+        </ul>
+        <h4><em>Promotion and Relegation</em></h4>
+        <ul>
+          <li>From 1892-93 to 1897-98, test matches were used to determine promotion and relegation between the First and Second Divisions.</li>
+          <li>These test matches took the form of a mini-league with each team from the First Division playing 2-legged ties against each team in the Second Division.</li>
+          <li>First Division teams winning the mini-league retained their status, whilst Second Division teams losing the mini-league stayed in the Second Division.</li>
+          <li>Second Division teams winning the mini-league had to apply for entry to the First Division whilst First Division teams losting the mini-league were offered a place in the Second Division.</li>
+        </ul>
+        <ul>
+          <li>From 1888-89 to 1985-86, a system of election/re-election was used to determine if clubs finishing bottom of the lowest division should be replaced in the League with non-League clubs.</li>
+          <li>Clubs in the bottom division of the League were required to reapply if they finished in the:
+            <ul>
+              <li>bottom 4 (1888-89 to 1892-93)</li>
+              <li>bottom 3 (1893-94 to 1920-21)</li>
+              <li>bottom 2 of either Division Three North or South (1921-22 to 1957-58)</li>
+              <li>bottom 4 (1958-59 - 1985-86)</li>
+            </ul>
+          </li>
+          <li>Since 1986-87, the election system has been replaced with automatic relegation.</li>
+        </ul>
+        <ul>
+          <li>Play-offs to determine which additional team would be promoted from each division were introduced from the 1986-87 season.</li>
+          <li>Initially, these involved one team from the higher division and 3 from the lower division.</li>
+          <li>From 1988-89 however, the play-offs were made up of 4 teams from the lower division only.</li>
+          <li>From the 1989-90 season, the play-off final was changed from 2-legged to 1-legged.</li>
+        </ul>
+      </div>
     );
   }
 }
