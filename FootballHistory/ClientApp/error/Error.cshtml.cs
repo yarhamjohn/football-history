ceae6c21efc1149f7339140a52cfb0dde38adb2b
John Yarham <yarhamjohn@gmail.com>
2019-01-18 15:08:14 +0000

    Move error cshtml files

--- FootballHistory/ClientApp/error/Error.cshtml.cs
+++ FootballHistory/ClientApp/error/Error.cshtml.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FootballHistory.Pages
+namespace FootballHistory.ClientApp.error
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
