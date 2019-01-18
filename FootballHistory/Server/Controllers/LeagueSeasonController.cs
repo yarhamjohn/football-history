f893ddd81b95a3482138baaeb9e0d6cd9601954e
John Yarham <yarhamjohn@gmail.com>
2019-01-18 16:09:40 +0000

    Clean the controller

--- FootballHistory/Server/Controllers/LeagueSeasonController.cs
+++ FootballHistory/Server/Controllers/LeagueSeasonController.cs
@@ -1,56 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FootballHistory.Server.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using static FootballHistory.Server.Repositories.LeagueSeasonRepository;
 
-namespace FootballHistory.Controllers
+namespace FootballHistory.Server.Controllers
 {
     [Route("api/[controller]")]
     public class LeagueSeasonController : Controller
     {
-        public ILeagueSeasonRepository m_Repository { get; }
+        private ILeagueSeasonRepository Repository { get; }
         public LeagueSeasonController(ILeagueSeasonRepository repository)
         {
-            m_Repository = repository;
+            Repository = repository;
         }
 
         [HttpGet("[action]")]
         public FilterOptions GetFilterOptions()
         {
-            return m_Repository.GetFilterOptions();
+            return Repository.GetFilterOptions();
         }
         
         [HttpGet("[action]")]
         public DefaultFilter GetDefaultFilter()
         {
-            return m_Repository.GetDefaultFilter();
+            return Repository.GetDefaultFilter();
         }
                 
         [HttpGet("[action]")]
         public List<ResultMatrixRow> GetResultMatrix(string tier, string season)
         {
-            return m_Repository.GetResultMatrix(Convert.ToInt32(tier), season);
+            return Repository.GetResultMatrix(Convert.ToInt32(tier), season);
         }
                                 
         [HttpGet("[action]")]
         public PlayOffs GetPlayOffMatches(string tier, string season)
         {
-            return m_Repository.GetPlayOffMatches(Convert.ToInt32(tier), season);
+            return Repository.GetPlayOffMatches(Convert.ToInt32(tier), season);
         }
                 
         [HttpGet("[action]")]
         public List<LeagueTableRow> GetLeagueTable(string tier, string season)
         {
-            return m_Repository.GetLeagueTable(Convert.ToInt32(tier), season);
+            return Repository.GetLeagueTable(Convert.ToInt32(tier), season);
         }
                 
         [HttpGet("[action]")]
         public LeagueRowDrillDown GetDrillDown(string tier, string season, string team)
         {
-            return m_Repository.GetDrillDown(Convert.ToInt32(tier), season, team);
+            return Repository.GetDrillDown(Convert.ToInt32(tier), season, team);
         }
     }
 }
