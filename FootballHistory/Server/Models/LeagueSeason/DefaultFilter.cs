00430b949d758250aaa18ffddb2a8aad33b57cba
John Yarham <yarhamjohn@gmail.com>
2019-01-25 15:27:59 +0000

    Remove unused classes

--- FootballHistory/Server/Models/LeagueSeason/DefaultFilter.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public class DefaultFilter
-{
-    public string Season { get; set; }
-    public Tier Tier { get; set; }
-}
--- FootballHistory/Server/Models/LeagueSeason/FilterOptions.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public class FilterOptions
-{
-    public List<string> AllSeasons { get; set; }
-    public List<Tier> AllTiers { get; set; }
-}
