565ede5054a231451331cdc2e7f589ea2ebedabd
John Yarham <yarhamjohn@gmail.com>
2019-02-01 10:31:37 +0000

    Rename Frontend project to FootballHistory.UI

--- FootballHistory.UI/Startup.cs
+++ FootballHistory.UI/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Frontend
+namespace FootballHistory.UI
 {
     public class Startup
     {
