a7a9584f90943ac31de90b81abacb76b92e4400c
John Yarham <yarhamjohn@gmail.com>
2019-02-05 21:29:31 +0000

    Pull matrix classes into new rows

--- FootballHistory.Api/Builders/Models/ResultMatrix.cs
+++ FootballHistory.Api/Builders/Models/ResultMatrix.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace FootballHistory.Api.Models.LeagueSeason
@@ -11,26 +10,5 @@ namespace FootballHistory.Api.Models.LeagueSeason
         {
             Rows = new List<ResultMatrixRow>();
         }
-    }
-
-    public class ResultMatrixRow
-    {
-        public string HomeTeam { get; set; }
-        public string HomeTeamAbbreviation { get; set; }
-        public List<ResultMatrixMatch> Results { get; set; }
-
-        public ResultMatrixRow()
-        {
-            Results = new List<ResultMatrixMatch>();
-        }
-    }
-
-    public class ResultMatrixMatch
-    {
-        public string AwayTeam { get; set; }
-        public string AwayTeamAbbreviation { get; set; }
-        public int? HomeScore { get; set; }
-        public int? AwayScore { get; set; }
-        public DateTime? MatchDate { get; set; }
     }
 }
---
+++ FootballHistory.Api/Builders/Models/ResultMatrixRow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace FootballHistory.Api.Models.LeagueSeason
+{
+    public class ResultMatrixRow
+    {
+        public string HomeTeam { get; set; }
+        public string HomeTeamAbbreviation { get; set; }
+        public List<ResultMatrixMatch> Results { get; set; }
+
+        public ResultMatrixRow()
+        {
+            Results = new List<ResultMatrixMatch>();
+        }
+    }
+}
