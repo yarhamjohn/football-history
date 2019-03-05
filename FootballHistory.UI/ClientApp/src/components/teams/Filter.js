0f3bd97d3cddd4f4a024d0d26c11b10c160880e0
John Yarham <yarhamjohn@gmail.com>
2019-03-05 16:18:46 +0000

    move api call to page render

--- FootballHistory.UI/ClientApp/src/components/teams/Filter.js
+++ FootballHistory.UI/ClientApp/src/components/teams/Filter.js
@@ -1,21 +1,13 @@
 import React, {useState, useEffect} from "react";
 import {ButtonToolbar, DropdownButton, MenuItem} from "react-bootstrap";
-import baseUrl from "../../api/LeagueSeasonApi";
 
 function Filter(props) {
-    const [allTeams, setAllTeams] = useState([]);
+    const [allTeams, setAllTeams] = useState(props.allTeams);
     const [selectedTeam, setSelectedTeam] = useState(props.selectedTeam);
     
     useEffect(() => {
-        fetch(`${baseUrl}/api/Team/GetTeamFilters`)
-            .then(response => response.json())
-            .then(data => { 
-                setAllTeams(data);
-                
-                let sorted = data.sort((a, b) => a > b);
-                let team = sorted.length > 0 ? sorted[0] : "";
-                updateSelectedTeam(team);
-            }); 
+        setAllTeams(props.allTeams);
+        setSelectedTeam(props.selectedTeam);
     }, []);
     
     const updateSelectedTeam = (team) => {
--- FootballHistory.UI/ClientApp/src/components/teams/Page.js
+++ FootballHistory.UI/ClientApp/src/components/teams/Page.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from "./Filter";
+import baseUrl from "../../api/LeagueSeasonApi";
 
 function Page() {
-  const [selectedTeam, setSelectedTeam] = useState("");
+    const [selectedTeam, setSelectedTeam] = useState("");
+    const [allTeams, setAllTeams] = useState([]);
 
+    useEffect(() => {
+        fetch(`${baseUrl}/api/Team/GetTeamFilters`)
+            .then(response => response.json())
+            .then(data => {
+                setAllTeams(data);
+    
+                let sorted = data.sort((a, b) => a > b);
+                let team = sorted.length > 0 ? sorted[0] : "";
+                setSelectedTeam(team);
+            });
+    }, []);
+    
   return (
       <div>
         <h1>{selectedTeam}</h1>
-          <Filter updateSelectedTeam={(team) => setSelectedTeam(team)} selectedTeam={selectedTeam}/>
+          <Filter updateSelectedTeam={(team) => setSelectedTeam(team)} selectedTeam={selectedTeam} allTeams={allTeams}/>
       </div>
   );
 }
