f3572d2d0ffb1e77079a5a03c09c9ab6efce1720
John Yarham <yarhamjohn@gmail.com>
2019-03-20 10:58:28 +0000

    Revise LeagueTableTests to add failing tests for league sorting rules
    Discovered Goals For took precedence over Goal Difference in the Football League (not Premier League) between 1992-93 and 1998-99.

--- FootballHistory.Api.UnitTests/LeagueSeason/LeagueTable/LeagueTableTests.cs
+++ FootballHistory.Api.UnitTests/LeagueSeason/LeagueTable/LeagueTableTests.cs
@@ -11,75 +11,120 @@ namespace FootballHistory.Api.UnitTests.LeagueSeason.LeagueTable
     [TestFixture]
     public class LeagueTableTests
     {
-        [Test]
-        public void AddPositionsAndStatuses_AddsPositionToEachRow_BasedOnPoints()
+        private readonly List<MatchDetailModel> _noPlayOffMatches = new List<MatchDetailModel>();
+
+        private Api.LeagueSeason.LeagueTable.LeagueTable _leagueTable;
+        private List<(int, string)> _sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName;
+        private List<(int, string)> _sortedByPointsThenGoalsForThenGoalDifferenceThenTeamName;
+
+        [SetUp]
+        public void Setup()
         {
-            var leagueTable = new Api.LeagueSeason.LeagueTable.LeagueTable
-            {
-                Rows = new List<LeagueTableRow>
+            _leagueTable = new Api.LeagueSeason.LeagueTable.LeagueTable
                 {
-                    new LeagueTableRow {Team = "Team1", Points = 3},
-                    new LeagueTableRow {Team = "Team2", Points = 1},
-                    new LeagueTableRow {Team = "Team3", Points = 2}
-                }
-            };
+                    Rows = new List<LeagueTableRow>
+                    {
+                        new LeagueTableRow {Team = "Team1", Points = 3, GoalDifference = 0, GoalsFor = 0},
+                        new LeagueTableRow {Team = "Team2", Points = 3, GoalDifference = 0, GoalsFor = 0},
+                        new LeagueTableRow {Team = "Team3", Points = 1, GoalDifference = 2, GoalsFor = 1},
+                        new LeagueTableRow {Team = "Team4", Points = 1, GoalDifference = 1, GoalsFor = 2},
+                        new LeagueTableRow {Team = "Team5", Points = 2, GoalDifference = -1, GoalsFor = -1}
+                    }
+                };
 
-            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3 };
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
-            
-            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
-            var expected = new List<(int, string)> { (1, "Team1"), (2, "Team3"), (3, "Team2") };
+            _sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName =
+                new List<(int, string)> { (1, "Team1"), (2, "Team2"), (3, "Team5"), (4, "Team3"), (5, "Team4") };
 
-            Assert.That(actual, Is.EqualTo(expected));
+            _sortedByPointsThenGoalsForThenGoalDifferenceThenTeamName =
+                new List<(int, string)> { (1, "Team1"), (2, "Team2"), (3, "Team5"), (4, "Team4"), (5, "Team3") };
         }
-        
+
         [Test]
-        public void AddPositionsAndStatuses_AddsPositionToEachRow_BasedOnPointsThenGoalDifference()
+        public void AddPositionsAndStatuses_SortsTopTier_ByPointsThenGoalDifferenceThenGoalsForThenTeamName_Between1992And2017([Range(1992, 2017, 1)] int seasonStartYear)
         {
-            var leagueTable = new Api.LeagueSeason.LeagueTable.LeagueTable
-            {
-                Rows = new List<LeagueTableRow>
-                {
-                    new LeagueTableRow {Team = "Team1", Points = 2, GoalDifference = 1},
-                    new LeagueTableRow {Team = "Team2", Points = 1, GoalDifference = 2},
-                    new LeagueTableRow {Team = "Team3", Points = 1, GoalDifference = 3}
-                }
-            };
-
-            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3 };
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = "Premier League", Season = season};
             
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
             var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
-            var expected = new List<(int, string)> { (1, "Team1"), (2, "Team3"), (3, "Team2") };
 
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName));
         }
-        
+                
         [Test]
-        public void AddPositionsAndStatuses_AddsPositionToEachRow_BasedOnPointsThenGoalDifferenceThenGoalsFor()
+        public void AddPositionsAndStatuses_SortsSecondTier_ByPointsThenGoalsForThenGoalDifferenceThenTeamName_Between1992And1998([Range(1992, 1998, 1)] int seasonStartYear)
         {
-            var leagueTable = new Api.LeagueSeason.LeagueTable.LeagueTable
-            {
-                Rows = new List<LeagueTableRow>
-                {
-                    new LeagueTableRow {Team = "Team1", Points = 1, GoalDifference = 2, GoalsFor = 1},
-                    new LeagueTableRow {Team = "Team2", Points = 1, GoalDifference = 1, GoalsFor = 2},
-                    new LeagueTableRow {Team = "Team3", Points = 1, GoalDifference = 1, GoalsFor = 3}
-                }
-            };
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = "First Division", Season = season};
 
-            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3 };
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
-            
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
             var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
-            var expected = new List<(int, string)> { (1, "Team1"), (2, "Team3"), (3, "Team2") };
 
-            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalsForThenGoalDifferenceThenTeamName));
         }
-                       
+                        
+        [Test]
+        public void AddPositionsAndStatuses_SortsThirdTier_ByPointsThenGoalsForThenGoalDifferenceThenTeamName_Between1992And1998([Range(1992, 1998, 1)] int seasonStartYear)
+        {
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = "Second Division", Season = season};
+            
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
+            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
+
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalsForThenGoalDifferenceThenTeamName));
+        }
+                                
+        [Test]
+        public void AddPositionsAndStatuses_SortsFourthTier_ByPointsThenGoalsForThenGoalDifferenceThenTeamName_Between1992And1998([Range(1992, 1998, 1)] int seasonStartYear)
+        {
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = "Third Division", Season = season};
+            
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
+            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
+
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalsForThenGoalDifferenceThenTeamName));
+        }                
+        [Test]
+        public void AddPositionsAndStatuses_SortsSecondTier_ByPointsThenGoalDifferenceThenGoalsForThenTeamName_Between1999And2017([Range(1999, 2017, 1)] int seasonStartYear)
+        {
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var competition = seasonStartYear < 2004 ? "First Division" : "Championship";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = competition, Season = season};
+            
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
+            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
+
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName));
+        }
+                        
+        [Test]
+        public void AddPositionsAndStatuses_SortsThirdTier_ByPointsThenGoalDifferenceThenGoalsForThenTeamName_Between1992And2017([Range(1999, 2017, 1)] int seasonStartYear)
+        {
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var competition = seasonStartYear < 2004 ? "Second Division" : "League One";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = competition, Season = season};
+
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
+            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
+
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName));
+        }
+                                
+        [Test]
+        public void AddPositionsAndStatuses_SortsFourthTier_ByPointsThenGoalDifferenceThenGoalsForThenTeamName_Between1992And2017([Range(1999, 2017, 1)] int seasonStartYear)
+        {
+            var season = $"{seasonStartYear} - {seasonStartYear + 1}";
+            var competition = seasonStartYear < 2004 ? "Third Division" : "League Two";
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 5, Competition = competition, Season = season};
+            
+            var leagueTableWithPositions = _leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
+            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
+
+            Assert.That(actual, Is.EqualTo(_sortedByPointsThenGoalDifferenceThenGoalsForThenTeamName));
+        }
+
         [Test]
         public void AddPositionsAndStatuses_AddsEmptyStatusToTeamsThatFinishedInAStandardPosition()
         {
@@ -93,35 +138,11 @@ namespace FootballHistory.Api.UnitTests.LeagueSeason.LeagueTable
                 }
             };
 
-            var leagueDetailModel = new LeagueDetailModel {TotalPlaces = 3};
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
+            var leagueDetailModel = new LeagueDetailModel {TotalPlaces = 3, Competition = "Test", Season = "0000 - 0000"};
+            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
 
             var actual = leagueTableWithPositions.Rows.Where(r => r.Status == string.Empty).Select(r => (r.Team, r.Position)).ToList();
             var expected = new List<(string, int)> { ("Team2", 2), ("Team3", 3) };
-            Assert.That(actual, Is.EqualTo(expected));
-        }
-        
-        [Test]
-        public void AddPositionsAndStatuses_AddsPositionToEachRow_BasedOnPointsThenGoalDifferenceThenGoalsForThenTeamName()
-        {
-            var leagueTable = new Api.LeagueSeason.LeagueTable.LeagueTable
-            {
-                Rows = new List<LeagueTableRow>
-                {
-                    new LeagueTableRow {Team = "Team3", Points = 1, GoalDifference = 1, GoalsFor = 2},
-                    new LeagueTableRow {Team = "Team1", Points = 1, GoalDifference = 1, GoalsFor = 1},
-                    new LeagueTableRow {Team = "Team2", Points = 1, GoalDifference = 1, GoalsFor = 1}
-                }
-            };
-            
-            var leagueDetailModel = new LeagueDetailModel {TotalPlaces = 3};
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
-            
-            var actual = leagueTableWithPositions.Rows.Select(r => (r.Position, r.Team)).ToList();
-            var expected = new List<(int, string)> { (1, "Team3"), (2, "Team1"), (3, "Team2") };
-
             Assert.That(actual, Is.EqualTo(expected));
         }
 
@@ -182,9 +203,8 @@ namespace FootballHistory.Api.UnitTests.LeagueSeason.LeagueTable
                 }
             };
 
-            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3, PromotionPlaces = 2};
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3, PromotionPlaces = 2, Competition = "Test", Season = "0000 - 0000"};
+            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
 
             var actual = leagueTableWithPositions.Rows.Where(r => r.Status == "P").Select(r => (r.Team, r.Position)).ToList();
             var expected = new List<(string, int)> { ("Team2", 2) };
@@ -204,9 +224,8 @@ namespace FootballHistory.Api.UnitTests.LeagueSeason.LeagueTable
                 }
             };
 
-            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3, RelegationPlaces = 1};
-            var playOffMatches = new List<MatchDetailModel>();
-            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, playOffMatches);
+            var leagueDetailModel = new LeagueDetailModel { TotalPlaces = 3, RelegationPlaces = 1, Competition = "Test", Season = "0000 - 0000"};
+            var leagueTableWithPositions = leagueTable.AddPositionsAndStatuses(leagueDetailModel, _noPlayOffMatches);
 
             var actual = leagueTableWithPositions.Rows.Where(r => r.Status == "R").Select(r => (r.Team, r.Position)).ToList();
             var expected = new List<(string, int)> { ("Team3", 3) };
