c327313428cf1567eee3e645a5064ef53c4ba4c0
John Yarham <yarhamjohn@gmail.com>
2019-03-26 10:48:14 +0000

    Simplify filter component

--- FootballHistory.UI/ClientApp/src/components/seasons/Filter.js
+++ FootballHistory.UI/ClientApp/src/components/seasons/Filter.js
@@ -5,7 +5,8 @@ import DropdownButton from "../shared/DropdownButton";
 class Filter extends Component {
   updateTier(tier) {
     const { updateFilter, selectedSeason } = this.props;
-    updateFilter(this.getDivisionTier(tier), selectedSeason);
+    let divisionTier = this.getDivisionTier(tier);
+    updateFilter(divisionTier, selectedSeason);
   };
 
   updateSeason(season) {
@@ -13,29 +14,36 @@ class Filter extends Component {
     updateFilter(selectedDivisionTier, season);
   };
 
-  getDivisionInfo(tier) {
-    let divisionNames = tier.divisions.map(d => `${d.name} (${d.activeFrom} - ${d.activeTo})`);
-    return divisionNames.join(', ');
-  };
-  
   getDivisionTier(expandedTier) {
     const tier = expandedTier.split(":")[0].split(" ")[1];
     return this.props.allDivisionTiers.filter(dt => dt.tier === parseInt(tier))[0];
   }
 
+  getDropdownString(dt) {
+    return `Tier ${dt.tier}: ${this.getDivisionInfo(dt)}`;
+  }
+
+  getDivisionInfo(tier) {
+    let divisionNames = tier.divisions.map(d => `${d.name} (${d.activeFrom} - ${d.activeTo})`);
+    return divisionNames.join(', ');
+  };
+
   render() {
     const { allDivisionTiers, allSeasons, selectedDivisionTier, selectedSeason } = this.props;
+    let sortedSeasons = allSeasons.sort().reverse();
+    let divisions = allDivisionTiers.map(dt => this.getDropdownString(dt));
+    let selectedDivision = this.getDropdownString(selectedDivisionTier);
 
     return (
       <ButtonToolbar>
         <DropdownButton 
             buttonLabel="Division" 
-            dropdownItems={allDivisionTiers.map(dt => `Tier ${dt.tier}: ${this.getDivisionInfo(dt)}`)} 
-            currentSelection={this.getDivisionInfo(selectedDivisionTier)} 
+            dropdownItems={divisions} 
+            currentSelection={selectedDivision} 
             updateSelected={(t) => this.updateTier(t)} />
         <DropdownButton 
             buttonLabel="Season" 
-            dropdownItems={allSeasons.sort().reverse()} 
+            dropdownItems={sortedSeasons} 
             currentSelection={selectedSeason} 
             updateSelected={(s) => this.updateSeason(s)} />
       </ButtonToolbar>
