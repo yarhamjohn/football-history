f0d9da0e3538024c088b4b68791d89b8f2c6f696
John Yarham <yarhamjohn@gmail.com>
2020-05-06 21:47:37 +0100

    color the chart

--- football-history-ui/src/ClubPage/ResultsGrid.tsx
+++ football-history-ui/src/ClubPage/ResultsGrid.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from "react";
-import { useLeagueMatches } from "./useLeagueMatches";
+import { LeagueMatch, useLeagueMatches } from "./useLeagueMatches";
 import { Table } from "semantic-ui-react";
+import moment from "moment";
 
 const EmptyCell: FunctionComponent = () => {
   return <Table.Cell style={{ background: "rgba(0,0,0,.03)" }}></Table.Cell>;
@@ -30,16 +31,8 @@ const ResultsGrid: FunctionComponent<{ tier: number; seasonStartYear: number | u
       return null;
     }
 
-    const homeGames = allLeagueMatches
-      .filter((m) => m.homeTeamAbbreviation === teamAbbreviation)
-      .map((m) => {
-        return {
-          awayTeamAbbreviation: m.awayTeamAbbreviation,
-          result: `${m.homeGoals}-${m.awayGoals}`,
-        };
-      });
-
-    homeGames.push({ awayTeamAbbreviation: teamAbbreviation, result: "" });
+    const homeGames = allLeagueMatches.filter((m) => m.homeTeamAbbreviation === teamAbbreviation);
+    homeGames.push({ awayTeamAbbreviation: teamAbbreviation } as LeagueMatch);
     homeGames.sort((a, b) => {
       if (a.awayTeamAbbreviation > b.awayTeamAbbreviation) {
         return 1;
@@ -50,11 +43,33 @@ const ResultsGrid: FunctionComponent<{ tier: number; seasonStartYear: number | u
       return 0;
     });
 
+    const GetColor = (homeGoals: number, awayGoals: number) => {
+      if (homeGoals > awayGoals) {
+        return "#75B266";
+      }
+
+      if (homeGoals < awayGoals) {
+        return "#B26694";
+      }
+
+      return "#BFA67F";
+    };
+
     const cells = homeGames.map((x) => {
       if (x.awayTeamAbbreviation === teamAbbreviation) {
         return <EmptyCell key={x.awayTeamAbbreviation} />;
       } else {
-        return <Table.Cell key={x.awayTeamAbbreviation}>{x.result}</Table.Cell>;
+        return (
+          <Table.Cell
+            key={x.awayTeamAbbreviation}
+            style={{ cursor: "context-menu", backgroundColor: GetColor(x.homeGoals, x.awayGoals) }}
+            title={`${moment(x.date).format("ddd, Do MMM YYYY")}: ${x.homeTeam} ${x.homeGoals} - ${
+              x.awayGoals
+            } ${x.awayTeam}`}
+          >
+            {`${x.homeGoals}-${x.awayGoals}`}
+          </Table.Cell>
+        );
       }
     });
 
