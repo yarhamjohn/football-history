61a5a5a6c12ec243c28497c2f6dd9e496e4ae798
yarhamjohn <yarhamjohn@gmail.com>
2020-05-10 14:52:10 +0100

    add skeleton redux

--- football-history-ui/src/index.tsx
+++ football-history-ui/src/index.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { AppContainer } from "react-hot-loader";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+type ProjectState = {
+  type: "LOADING_PROJECT";
+};
+
+type ProjectAction = {
+  type: "PROJECT_LOAD_STARTED";
+};
+
+interface ApplicationState {
+  projectState: ProjectState;
+}
+type ApplicationAction = ProjectAction;
+
+const projectReducer = (
+  prevState: ProjectState | undefined,
+  action: ApplicationAction
+): ProjectState => {
+  if (!prevState) {
+    return { type: "LOADING_PROJECT" };
+  }
+
+  return prevState;
+};
+
+const applicationReducer = (
+  prevState: ApplicationState | undefined,
+  action: ApplicationAction
+): ApplicationState => {
+  return {
+    projectState: projectReducer(prevState?.projectState, action),
+  };
+};
+
+const store = createStore<ApplicationState, ApplicationAction, unknown, unknown>(
+  applicationReducer
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <AppContainer>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AppContainer>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
