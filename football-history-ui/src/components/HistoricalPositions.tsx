e5f5a5b6135f9d5232f1fc03ab4e7b6af816ac5e
John Yarham <yarhamjohn@gmail.com>
2020-05-22 21:15:48 +0100

    try a work-around 2

--- football-history-ui/src/components/HistoricalPositions.tsx
+++ football-history-ui/src/components/HistoricalPositions.tsx
@@ -5,14 +5,55 @@ import { ResponsiveLine, Layer, Serie } from "@nivo/line";
 const HistoricalPositions: FunctionComponent<{ selectedClub: string }> = ({ selectedClub }) => {
   const { historicalPositionsState } = useHistoricalPositions(selectedClub, 2000, 2015);
 
-  function getPositionData() {
-    return historicalPositionsState.type === "HISTORICAL_POSITIONS_LOAD_SUCCEEDED"
-      ? historicalPositionsState.positions
-          .map((p) => {
-            return { year: p.seasonStartYear, position: p.absolutePosition, status: p.status };
-          })
-          .sort()
-      : [];
+  function relegatedInSeason(season: number) {
+    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
+      return false;
+    }
+    const positions = historicalPositionsState.positions.filter(
+      (p) => p.seasonStartYear === season
+    );
+
+    if (positions.length !== 1) {
+      return false;
+    }
+
+    return positions[0].status === "Relegated";
+  }
+  function promotedInSeason(season: number) {
+    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
+      return false;
+    }
+    const positions = historicalPositionsState.positions.filter(
+      (p) => p.seasonStartYear === season
+    );
+
+    if (positions.length !== 1) {
+      return false;
+    }
+
+    return (
+      positions[0].status === "Promoted" ||
+      positions[0].status === "Champions" ||
+      positions[0].status === "PlayOff Winner"
+    );
+  }
+
+  function clubIsInTier(seasonStartYear: number, tier: number) {
+    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
+      return false;
+    }
+
+    return historicalPositionsState.positions.some(
+      (p) => p.seasonStartYear === seasonStartYear && p.tier === tier
+    );
+  }
+
+  function clubWasRelegatedToTier(season: number, tier: number) {
+    return relegatedInSeason(season) && clubIsInTier(season, tier - 1);
+  }
+
+  function clubWasPromotedToTier(season: number, tier: number) {
+    return promotedInSeason(season) && clubIsInTier(season, tier + 1);
   }
 
   const getDates = (start: number, end: number) =>
@@ -34,13 +75,11 @@ const HistoricalPositions: FunctionComponent<{ selectedClub: string }> = ({ sele
         data: allDates.map((d) => {
           return {
             x: d,
-            y: historicalPositionsState.positions.some(
-              (p) => p.seasonStartYear === d && p.tier === t
-            )
-              ? historicalPositionsState.positions.filter(
-                  (p) => p.seasonStartYear === d && p.tier === t
-                )[0].absolutePosition
-              : null,
+            y:
+              clubIsInTier(d, t) || clubWasRelegatedToTier(d, t) || clubWasPromotedToTier(d, t)
+                ? historicalPositionsState.positions.filter((p) => p.seasonStartYear === d)[0]
+                    .absolutePosition
+                : null,
           };
         }),
       };
