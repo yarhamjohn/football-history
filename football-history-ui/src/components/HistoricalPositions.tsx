31bbfdc8855d3d8348ba0ae87ad8df513b77997d
John Yarham <yarhamjohn@gmail.com>
2020-05-22 21:27:19 +0100

    go with lines instead

--- football-history-ui/src/components/HistoricalPositions.tsx
+++ football-history-ui/src/components/HistoricalPositions.tsx
@@ -1,89 +1,58 @@
 import React, { FunctionComponent } from "react";
 import { useHistoricalPositions } from "../hooks/useHistoricalPositions";
-import { ResponsiveLine, Layer, Serie } from "@nivo/line";
+import { ResponsiveLine, Serie } from "@nivo/line";
 
 const HistoricalPositions: FunctionComponent<{ selectedClub: string }> = ({ selectedClub }) => {
   const { historicalPositionsState } = useHistoricalPositions(selectedClub, 2000, 2015);
-
-  function relegatedInSeason(season: number) {
-    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
-      return false;
-    }
-    const positions = historicalPositionsState.positions.filter(
-      (p) => p.seasonStartYear === season
-    );
-
-    if (positions.length !== 1) {
-      return false;
-    }
-
-    return positions[0].status === "Relegated";
-  }
-  function promotedInSeason(season: number) {
-    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
-      return false;
-    }
-    const positions = historicalPositionsState.positions.filter(
-      (p) => p.seasonStartYear === season
-    );
-
-    if (positions.length !== 1) {
-      return false;
-    }
-
-    return (
-      positions[0].status === "Promoted" ||
-      positions[0].status === "Champions" ||
-      positions[0].status === "PlayOff Winner"
-    );
-  }
-
-  function clubIsInTier(seasonStartYear: number, tier: number) {
-    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
-      return false;
-    }
-
-    return historicalPositionsState.positions.some(
-      (p) => p.seasonStartYear === seasonStartYear && p.tier === tier
-    );
-  }
-
-  function clubWasRelegatedToTier(season: number, tier: number) {
-    return relegatedInSeason(season) && clubIsInTier(season, tier - 1);
-  }
-
-  function clubWasPromotedToTier(season: number, tier: number) {
-    return promotedInSeason(season) && clubIsInTier(season, tier + 1);
-  }
 
   const getDates = (start: number, end: number) =>
     Array.from({ length: end - start }, (v, k) => k + start);
 
   function getData() {
     let series: Serie[] = [];
-    let colors = ["black", "red", "blue", "green"];
+    let colors = ["black", "#75B266", "#BFA67F", "#B26694"];
 
     if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
       return { series, colors };
     }
 
     const allDates = getDates(1999, 2016);
-    const tiers = Array.from(new Set(historicalPositionsState.positions.map((p) => p.tier)));
-    series = tiers.map((t) => {
-      return {
-        id: `tier${t}Positions`,
+
+    series = [
+      {
+        id: "positions",
         data: allDates.map((d) => {
           return {
             x: d,
-            y:
-              clubIsInTier(d, t) || clubWasRelegatedToTier(d, t) || clubWasPromotedToTier(d, t)
-                ? historicalPositionsState.positions.filter((p) => p.seasonStartYear === d)[0]
-                    .absolutePosition
-                : null,
+            y: historicalPositionsState.positions.some((p) => p.seasonStartYear === d)
+              ? historicalPositionsState.positions.filter((p) => p.seasonStartYear === d)[0]
+                  .absolutePosition
+              : null,
           };
         }),
-      };
-    });
+      },
+      {
+        id: "tier1-tier2",
+        data: [
+          { x: Math.min(...allDates), y: 20 },
+          { x: Math.max(...allDates), y: 20 },
+        ],
+      },
+      {
+        id: "tier2-tier3",
+        data: [
+          { x: Math.min(...allDates), y: 44 },
+          { x: Math.max(...allDates), y: 44 },
+        ],
+      },
+      {
+        id: "tier3-tier4",
+        data: [
+          { x: Math.min(...allDates), y: 68 },
+          { x: Math.max(...allDates), y: 68 },
+        ],
+      },
+    ];
 
     return { series, colors };
   }
