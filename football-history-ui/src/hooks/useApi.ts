6a56cfcfc412124235acf21a7f486b2a5cf12fd8
John Yarham <yarhamjohn@gmail.com>
2020-06-02 19:16:46 +0100

    use correct environment variable

--- football-history-ui/src/hooks/useApi.ts
+++ football-history-ui/src/hooks/useApi.ts
@@ -1,19 +1,5 @@
-import { useEffect, useState } from "react";
-
 const useApi = () => {
-  const devApiUrl = "https://localhost:5001";
-  const prodApiUrl = "https://footballhistoryapi.azurewebsites.net";
-
-  const [api, setApi] = useState<string>(devApiUrl);
-
-  useEffect(() => {
-    if (process.env.ASPNETCORE_ENVIRONMENT === "Development") {
-      setApi(devApiUrl);
-    } else {
-      setApi(prodApiUrl);
-    }
-  }, []);
-
+  const api = process.env.REACT_APP_FOOTBALL_HISTORY_URL;
   return { api };
 };
 
