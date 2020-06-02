18c98744bea8f6d4c910643243966419f9ab6a5d
John Yarham <yarhamjohn@gmail.com>
2020-06-02 19:37:08 +0100

    try again

--- football-history-ui/src/hooks/useApi.ts
+++ football-history-ui/src/hooks/useApi.ts
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 const useApi = () => {
-  const api = process.env.REACT_APP_FOOTBALL_HISTORY_URL;
+  const devApiUrl = "https://localhost:5001";
+  const prodApiUrl = "https://footballhistoryapi.azurewebsites.net";
+
+  const [api, setApi] = useState<string>(devApiUrl);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      setApi(devApiUrl);
+    } else {
+      setApi(prodApiUrl);
+    }
+  }, []);
+
   return { api };
 };
 
