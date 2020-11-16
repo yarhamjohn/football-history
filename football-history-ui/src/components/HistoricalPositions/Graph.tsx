80ef6753f3cbe153945a315a57bcff19edfb6f82
yarhamjohn <yarhamjohn@gmail.com>
2020-11-16 21:51:42 +0000

    wip: fix league boundary lines

--- football-history-ui/src/components/HistoricalPositions/Graph.tsx
+++ football-history-ui/src/components/HistoricalPositions/Graph.tsx
@@ -1,10 +1,105 @@
 import React, { FunctionComponent, useState } from "react";
-import { HistoricalPosition, useHistoricalPositions } from "../../hooks/useHistoricalPositions";
+import {
+    HistoricalPosition,
+    HistoricalPositionsState,
+    useHistoricalPositions,
+} from "../../hooks/useHistoricalPositions";
 import { Point, ResponsiveLine, Serie } from "@nivo/line";
 import { Season as SeasonType } from "../../hooks/useSeasons";
 import { YearSlider } from "../Filters/YearSlider";
 import { getLeagueStatusColor } from "../../shared/functions";
 import { Tooltip } from "./Tooltip";
+
+const getDates = (start: number, end: number) =>
+    Array.from({ length: end - start }, (v, k) => k + start);
+
+function getPositions(
+    allDates: number[],
+    historicalPositionsState: {
+        type: "HISTORICAL_POSITIONS_LOAD_SUCCEEDED";
+        positions: HistoricalPosition[];
+    }
+) {
+    return allDates.map((d) => {
+        return {
+            x: d,
+            y: historicalPositionsState.positions.some((p) => p.seasonStartYear === d)
+                ? historicalPositionsState.positions.filter((p) => p.seasonStartYear === d)[0]
+                      .absolutePosition
+                : null,
+        };
+    });
+}
+
+function getTierBottomBoundary(tier: 1 | 2 | 3, selectedFilterRange: number[]) {
+    //TODO: this should calculate automatically based on League model info but I think the api needs changing to get this easily
+
+    let result = [];
+    if (tier === 1) {
+        for (let i = selectedFilterRange[0]; i < selectedFilterRange[1]; i++) {
+            if ((i >= 1976 && i <= 1986) || (i >= 1991 && i <= 1994)) {
+                result.push({ x: i, y: 22.5 });
+            } else if (i === 1987) {
+                result.push({ x: i, y: 21.5 });
+            } else {
+                result.push({ x: i, y: 20.5 });
+            }
+        }
+    } else if (tier === 2) {
+        for (let i = selectedFilterRange[0]; i < selectedFilterRange[1]; i++) {
+            if (i >= 1991 && i <= 1994) {
+                result.push({ x: i, y: 46.5 });
+            } else {
+                result.push({ x: i, y: 44.5 });
+            }
+        }
+    } else if (tier === 3) {
+        for (let i = selectedFilterRange[0]; i < selectedFilterRange[1]; i++) {
+            if (i >= 1991 && i <= 1994) {
+                result.push({ x: i, y: 70.5 });
+            } else {
+                result.push({ x: i, y: 68.5 });
+            }
+        }
+    }
+
+    return result;
+}
+
+function getData(
+    selectedFilterRange: number[],
+    historicalPositionsState: HistoricalPositionsState
+) {
+    let series: Serie[] = [];
+    let colors = ["black", "#75B266", "#BFA67F", "#B26694"];
+
+    if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
+        return { series, colors };
+    }
+
+    const allDates = getDates(selectedFilterRange[0] - 1, selectedFilterRange[1] + 1);
+
+    series = [
+        {
+            id: "positions",
+            data: getPositions(allDates, historicalPositionsState),
+        },
+        {
+            id: "tier1-tier2",
+            data: getTierBottomBoundary(1, selectedFilterRange),
+        },
+        {
+            id: "tier2-tier3",
+            data: getTierBottomBoundary(2, selectedFilterRange),
+        },
+        {
+            id: "tier3-tier4",
+            data: getTierBottomBoundary(3, selectedFilterRange),
+        },
+    ];
+
+    return { series, colors };
+}
 
 const HistoricalPositionsGraph: FunctionComponent<{
     selectedClub: string;
@@ -24,81 +119,9 @@ const HistoricalPositionsGraph: FunctionComponent<{
     ]);
 
     const { historicalPositionsState } = useHistoricalPositions(selectedClub, selectedFilterRange);
+    const { series, colors } = getData(selectedFilterRange, historicalPositionsState);
 
-    const getDates = (start: number, end: number) =>
-        Array.from({ length: end - start }, (v, k) => k + start);
-
-    function getData() {
-        let series: Serie[] = [];
-        let colors = ["black", "#75B266", "#BFA67F", "#B26694"];
-
-        if (historicalPositionsState.type !== "HISTORICAL_POSITIONS_LOAD_SUCCEEDED") {
-            return { series, colors };
-        }
-
-        const allDates = getDates(selectedFilterRange[0] - 1, selectedFilterRange[1] + 1);
-
-        series = [
-            {
-                id: "positions",
-                data: allDates.map((d) => {
-                    return {
-                        x: d,
-                        y: historicalPositionsState.positions.some((p) => p.seasonStartYear === d)
-                            ? historicalPositionsState.positions.filter(
-                                  (p) => p.seasonStartYear === d
-                              )[0].absolutePosition
-                            : null,
-                    };
-                }),
-            },
-            {
-                //TODO: This data needs to be dynamically created based on the dates actually shown on the graph
-                id: "tier1-tier2",
-                data: [
-                    { x: 1976, y: 22.5 },
-                    { x: 1986, y: 22.5 },
-                    { x: 1987, y: 21.5 },
-                    { x: 1988, y: 20.5 },
-                    { x: 1990, y: 20.5 },
-                    { x: 1991, y: 22.5 },
-                    { x: 1994, y: 22.5 },
-                    { x: 1995, y: 20.5 },
-                    { x: 2019, y: 20.5 },
-                ],
-            },
-            {
-                id: "tier2-tier3",
-                data: [
-                    { x: 1976, y: 44.5 },
-                    { x: 1986, y: 44.5 },
-                    { x: 1990, y: 44.5 },
-                    { x: 1991, y: 46.5 },
-                    { x: 1994, y: 46.5 },
-                    { x: 1995, y: 44.5 },
-                    { x: 2019, y: 44.5 },
-                ],
-            },
-            {
-                id: "tier3-tier4",
-                data: [
-                    { x: 1976, y: 68.5 },
-                    { x: 1986, y: 68.5 },
-                    { x: 1990, y: 68.5 },
-                    { x: 1991, y: 70.5 },
-                    { x: 1994, y: 70.5 },
-                    { x: 1995, y: 68.5 },
-                    { x: 2019, y: 68.5 },
-                ],
-            },
-        ];
-
-        return { series, colors };
-    }
-
-    const { series, colors } = getData();
-
-    function getTicks() {
+    function getYAxisTicks() {
         return [1, 16, 31, 46, 61, 76, 92];
     }
 
@@ -117,7 +140,7 @@ const HistoricalPositionsGraph: FunctionComponent<{
                     colors={colors}
                     margin={{ left: 25, bottom: 25, top: 10 }}
                     yScale={{ type: "linear", min: 1, max: 92, reverse: true }}
-                    gridYValues={getTicks()}
+                    gridYValues={getYAxisTicks()}
                     enableSlices="x"
                     sliceTooltip={({ slice }) => {
                         return (
