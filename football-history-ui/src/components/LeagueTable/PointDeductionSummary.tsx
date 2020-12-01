2e668c0467be087c6c9562a0f7ac947bbf6a473d
John Yarham <yarhamjohn@gmail.com>
2020-12-01 21:54:14 +0000

    fix point deductions

--- football-history-ui/src/components/LeagueTable/PointDeductionSummary.tsx
+++ football-history-ui/src/components/LeagueTable/PointDeductionSummary.tsx
@@ -2,17 +2,17 @@ import React, { FunctionComponent } from "react";
 import { Row } from "../../hooks/useLeagueTable";
 
 const PointDeductionSummary: FunctionComponent<{ leagueTable: Row[] }> = ({ leagueTable }) => {
-  const pointDeductionRows = leagueTable.filter((r) => r.pointsDeducted > 0);
-  return (
-    <div>
-      {pointDeductionRows.map((r) => (
-        <p key={r.position}>
-          * {r.team}: {r.pointsDeducted} point{r.pointsDeducted === 1 ? "" : "s"} deducted -{" "}
-          {r.pointsDeductionReason}
-        </p>
-      ))}
-    </div>
-  );
+    const pointDeductionRows = leagueTable.filter((r) => r.pointsDeducted !== 0);
+    return (
+        <div>
+            {pointDeductionRows.map((r) => (
+                <p key={r.position}>
+                    * {r.team}: {r.pointsDeducted} point{r.pointsDeducted === 1 ? "" : "s"} deducted
+                    - {r.pointsDeductionReason}
+                </p>
+            ))}
+        </div>
+    );
 };
 
 export { PointDeductionSummary };
